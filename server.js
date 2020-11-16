b7b2cfecaff5e0b877c5aea812c54a8ca5c6a1b6
Gaurav Kundu <gauravkundu@Gauravs-MacBook-Pro.local>
2020-11-17 01:22:35 +0530

    updated the server.js file

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-//Install express server
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/space-x-spaient'));
+
+// app.get('/*', function(req,res) {
+    
+// res.sendFile(path.join(__dirname+'/dist/space-x-spaient/index.html'));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/space-x-spaient'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/space-x-spaient/index.html'));
+app.use(express.static('./dist/space-x-spaient'));
+app.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/space-x-spaient/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
